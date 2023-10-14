85bfa8156105b9cd7d14bf2b6f0ab09261f2edf5
Jasil Shan <jasilshan786@gmail.com>
2023-10-14 17:39:12 +0530

    component added

---
+++ src/Components/Weather/Weather.jsx
@@ -0,0 +1,19 @@
+import './Weather.css'
+import search from '../assets/search.png'
+import clear from '../assets/clear.png'
+import cloud from '../assets/cloud.png'
+import drizzle from '../assets/drizzle.png'
+import humidity from '../assets/humidity.png'
+import rain from '../assets/rain.png'
+import snow from '../assets/snow.png'
+import wind from '../assets/wind.png'
+
+
+
+const Weather = () => {
+  return (
+    <div>Weather</div>
+  )
+}
+
+export default Weather
